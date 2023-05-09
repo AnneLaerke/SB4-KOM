0c9cb7f69d60a03336f86b930f757693db41c3f6
AnneLaerke <91538760+AnneLaerke@users.noreply.github.com>
2023-05-09 11:54:14 +0200

    [SpringLab] reworks

--- AsteroidsEntityFramework/Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ AsteroidsEntityFramework/Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -1,7 +1,7 @@
 package dk.sdu.mmmi.cbse.main;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
@@ -9,13 +9,12 @@ public class Main {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringModuleConfiguration.class);
 		
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "Asteroids";
-		cfg.width = 500;
-		cfg.height = 400;
-		cfg.useGL30 = false;
-		cfg.resizable = false;
+		Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
 
-		new LwjglApplication(applicationContext.getBean(Game.class), cfg);
+		cfg.setTitle("Asteroids");
+		cfg.setResizable(false);
+		cfg.setWindowedMode(1200,1000);
+
+		new Lwjgl3Application(applicationContext.getBean(Game.class), cfg);
 	}
 }
--- AsteroidsEntityFramework/Core/src/main/java/module-info.java
+++ AsteroidsEntityFramework/Core/src/main/java/module-info.java
@@ -4,11 +4,10 @@ import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 
 module Core {
     requires Common;
-    requires gdx;
-    requires gdx.backend.lwjgl;
     requires spring.context;
     requires spring.core;
     requires spring.beans;
+    requires com.badlogic.gdx;
 
     opens dk.sdu.mmmi.cbse.main;
     exports dk.sdu.mmmi.cbse.main;
